c721566b5f3409b4d936e6b15a0e4731d290251b
naufal.sunandar <nauf.sunandar@gmail.com>
2025-10-21 15:30:43 +0700

    add back button to history

--- orange-pay/src/pages/HistoryTransactionPage.jsx
+++ orange-pay/src/pages/HistoryTransactionPage.jsx
@@ -2,11 +2,13 @@ import HeaderSection from "../components/dashboard/HeaderSection";
 import BalanceCard from "../components/history_transaksi/BalanceCard.jsx";
 import RecentHistory from "../components/history_transaksi/RecentHistory.jsx";
 import DynamicShell from "../components/layout/dynamicShell";
+import PageHeader from "../components/page_header/PageHeader.jsx";
 
 export default function HistoryTransactionPage() {
   return (
     <DynamicShell>
       <HeaderSection />
+      <PageHeader>Transaction History</PageHeader>
       <BalanceCard />
       <RecentHistory />
     </DynamicShell>
