7f2c9c51efc2854480fdc6c9014f6db1ea64891f
naufal.sunandar <nauf.sunandar@gmail.com>
2025-10-28 22:51:05 +0700

    update getUserName

--- orange-pay/vite.config.js
+++ orange-pay/vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
+      // Auth service (8081)
+      '/api/v1/auth': {
         target: 'http://localhost:8081',
+        changeOrigin: true,
+        secure: false,
+      },
+
+      // User service (8082)
+      '/api/v1/user': {
+        target: 'http://localhost:8082',
         changeOrigin: true,
         secure: false,
       },
