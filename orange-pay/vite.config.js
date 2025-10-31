bf6bfeab1037e25e582a4af9cf7c96addd25f43f
rully <rully.rosadi.20@student.it.ittelkom-sby.ac.id>
2025-10-31 13:44:47 +0700

    change vite

--- orange-pay/vite.config.js
+++ orange-pay/vite.config.js
@@ -6,35 +6,35 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/v1/auth": {
-        target: "http://localhost:8081",
+        target: "https://api-dev.orangebybni.my.id:8080",
         changeOrigin: true,
         secure: false,
       },
       "/api/v1/wallets": {
-        target: "http://localhost:8080",
+        target: "https://api-dev.orangebybni.my.id:8080",
         changeOrigin: true,
         secure: false,
       },
       //default receive wallet
       "/api/v1/users/me": {
-        target: "http://localhost:8080",
+        target: "https://api-dev.orangebybni.my.id:8080",
         changeOrigin: true,
         secure: false,
       },
       //limit
       "/api/v1/users/me/limits": {
-        target: "http://localhost:8080",
+        target: "https://api-dev.orangebybni.my.id:8080",
         changeOrigin: true,
         secure: false,
       },
       "/api/v1/invites": {
-        target: "http://localhost:8080",
+        target: "https://api-dev.orangebybni.my.id:8080",
         changeOrigin: true,
         secure: false,
       },
       // User service (8082)
       "/api/v1/user": {
-        target: "http://localhost:8082",
+        target: "https://api-dev.orangebybni.my.id:8080",
         changeOrigin: true,
         secure: false,
       },
