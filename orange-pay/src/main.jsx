f173992f726aa2eb5bcf508a881bab7ca4421fb9
StevenSiahaann <88304745+StevenSiahaann@users.noreply.github.com>
2025-11-03 02:31:05 +0700

    feat: add clarity config

--- orange-pay/src/main.jsx
+++ orange-pay/src/main.jsx
@@ -3,9 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import "./index.css";
+import { useEffect } from "react";
+import { loadClarity } from "./util/clarity.js";
+const CLARITY_ID = import.meta.env.VITE_CLARITY_PROJECT_ID;
 
+function Root() {
+  const hasConsent = true;
+
+  useEffect(() => {
+    if (import.meta.env.PROD && hasConsent) {
+      loadClarity(CLARITY_ID);
+    }
+  }, [hasConsent]);
+
+  return <App />;
+}
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
