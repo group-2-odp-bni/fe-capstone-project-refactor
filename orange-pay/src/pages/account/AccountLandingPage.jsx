dda881fd47c268281f3bd1a84d51acc8d40a197b
naufal.sunandar <nauf.sunandar@gmail.com>
2025-11-04 17:40:05 +0700

    fix : account landing page

--- orange-pay/src/pages/account/AccountLandingPage.jsx
+++ orange-pay/src/pages/account/AccountLandingPage.jsx
@@ -1,46 +1,60 @@
 import { useNavigate } from "react-router-dom";
-import ButtonLink from "../../components/button/ButtonLink";
 import MobileShell from "../../components/layout/MobileShell";
 import PhoneLayoutBackground from "../../components/PhoneLayoutBackground";
 import OrangeHeader from "../../components/register/OrangeHeader";
 import WhiteCardContainer from "../../components/register/WhiteCardContainer";
+import WhiteHeader from "../../components/register/WhiteHeader";
+import ContentBox from "../../components/common/ContentBox";
+import { FullActionButton } from "../../components/button/FullActionButton";
+import { clearTokens } from "../../services/auth/authService";
+import AccountButtonLink from "../../components/button/AccountButtonLink";
+
 
 export default function AccountLandingPage() {
     const navigate = useNavigate();
+    const handleLogout = () => {
+        clearTokens();
+        navigate("/login", { replace: true });
+    };
 
     return (
         <PhoneLayoutBackground>
             <MobileShell>
-                <OrangeHeader />
-                <WhiteCardContainer>
-                    <div className="text-center">
-                        <h2 className="text-2xl font-semibold text-gray-800 mb-6">
-                            Akun Saya
-                        </h2>
+                <WhiteHeader title="Account" />
+                <ContentBox className="border border-gray-300">
+                    <div className="">
+
+
 
                         <div className="space-y-6">
+                            <h2 className="m-1 font-medium">Akun Saya</h2>
 
-                            <div className="border border-gray-200 rounded-xl p-3 shadow-sm hover:shadow-md transition-all duration-200">
-                                <ButtonLink
-                                    onClick={() => navigate("/app/profile")}
-                                    className="text-[#1C6C79]"
-                                >
-                                    Lihat Profil
-                                </ButtonLink>
+
+                            <AccountButtonLink
+                                onClick={() => navigate("/app/profile")}
+                            >
+                                Lihat Profil
+                            </AccountButtonLink>
+
+
+
+                            <h2 className="m-1">Kelola Batas</h2>
+                            <AccountButtonLink
+                                onClick={() => navigate("/app/transactionLimit")}
+                            >
+                                Kelola Batas
+                            </AccountButtonLink>
+
+                            <div className="mt-auto mb-6 flex justify-center">
+                                <FullActionButton onClick={handleLogout}>Keluar</FullActionButton>
                             </div>
 
-
-                            <div className="border border-gray-200 rounded-xl p-3 shadow-sm hover:shadow-md transition-all duration-200">
-                                <ButtonLink
-                                    onClick={() => navigate("/app/transactionLimit")}
-                                    className="text-[#1C6C79] hover:no-underline"
-                                >
-                                    Kelola Batas
-                                </ButtonLink>
-                            </div>
                         </div>
                     </div>
-                </WhiteCardContainer>
+
+
+                </ContentBox>
+
             </MobileShell>
         </PhoneLayoutBackground>
     );
