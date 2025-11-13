6ab26ee81d2bea894739d12f545fcd6fabb71c4a
StevenSiahaann <88304745+StevenSiahaann@users.noreply.github.com>
2025-11-13 08:42:18 +0700

    fix: minor fix on import index js

--- orange-pay/src/routes/index.jsx
+++ orange-pay/src/routes/index.jsx
@@ -18,14 +18,11 @@ import SplitBillPage from "../pages/SplitBillPage";
 import SplitBillConfirmedPage from "../pages/SplitBillConfirmedPage";
 import SplitBillMemberPage from "../pages/SplitBillMemberPage";
 import SplitBillReviewPage from "../pages/SplitBillReviewPage";
-import OtpStage from "../components/login/OtpStage";
-import PinStage from "../components/login/PinStage";
 import ResetSetPin from "../components/login/ResetSetPin";
 import ResetPin from "../components/login/ResetPin";
 import ResetPinOtp from "../components/login/ResetPinOtp";
 import DashboardPage from "../pages/DashboardPage";
 import HistoryTransactionPage from "../pages/HistoryTransactionPage";
-import { isAuthenticated } from "../services/authService";
 import { RegistrationProvider } from "../context/RegistrationContext";
 import AddWalletPage from "../pages/AddWalletPage";
 import AllHistoryPage from "../pages/AllHistory";
@@ -38,7 +35,6 @@ import { AddBalanceProvider } from "../context/AddBalanceContext";
 // import ReceiptPage from "../pages/ReceiptPage";
 import ReceiptPage from "../pages/ReceiptPage";
 import InviteClaimPage from "../pages/InviteClaimsPage";
-import AddBalancePinPage from "../pages/AddBalancePinPage";
 /* login flow context & step guard */
 import { LoginProvider } from "../context/LoginContext";
 import RequireLoginStep from "./RequireLoginStep";
@@ -149,7 +145,7 @@ function AddBalancelayout() {
     <AddBalanceProvider>
       <Outlet />
     </AddBalanceProvider>
-  )
+  );
 }
 
 /* ----------------- Routes ----------------- */
@@ -225,17 +221,20 @@ export default function AppRoutes() {
               <AssignMemberPage walletIdOverride="d69f4f9d-ec91-4d43-8db0-3006185c1090" />
             }
           />
-          <Route path="wallets/:walletId/members" element={<AssignMemberPage />} />
-
+          <Route
+            path="wallets/:walletId/members"
+            element={<AssignMemberPage />}
+          />
           {/* split bill */}
           <Route path="splitbill/review" element={<SplitBillReviewPage />} />
           <Route path="splitbill" element={<SplitBillPage />} />
           <Route path="splitbill/:id" element={<SplitBillConfirmedPage />} />
-          <Route path="splitbill/:id/member/:memberId" element={<SplitBillMemberPage />} />{" "}
-
+          <Route
+            path="splitbill/:id/member/:memberId"
+            element={<SplitBillMemberPage />}
+          />{" "}
           {/* account page */}
           <Route path="account" element={<AccountLandingPage />} />
-
           {/* topup page */}
           <Route path="topup" element={<TopupLayout />}>
             <Route index element={<TopUpPage />} />
@@ -243,7 +242,6 @@ export default function AppRoutes() {
             <Route path="confirm" element={<TopUpConfirmationPage />} />
             <Route path="result" element={<TopupResultPage />} />
           </Route>
-
           {/* user profile page */}
           <Route element={<ProfileLayout />}>
             <Route path="profile" element={<ProfilePage />} />
@@ -251,32 +249,33 @@ export default function AppRoutes() {
             <Route path="verifyEmail" element={<VerifyEmailPage />} />
             <Route path="verifyPhone" element={<VerifyPhonePage />} />
           </Route>
-
           {/* user transaction limit page */}
           <Route element={<TransactionLimitLayout />}>
             <Route path="transactionLimit" element={<TransactionLimitPage />} />
-            <Route path="edittransactionLimit" element={<TransactionLimitEditPage />} />
+            <Route
+              path="edittransactionLimit"
+              element={<TransactionLimitEditPage />}
+            />
           </Route>
-
           {/* add balance */}
           <Route element={<AddBalancelayout />}>
-            <Route path="wallets/confirm-add-balance" element={<ConfirmAddBalancePage />} />
+            <Route
+              path="wallets/confirm-add-balance"
+              element={<ConfirmAddBalancePage />}
+            />
             <Route path="wallets/:walletId/add" element={<AddBalancePage />} />
           </Route>
-
-
           <Route path="wallets">
             <Route path="new" element={<AddWalletPage />} />
             <Route path=":walletId" element={<HistoryTransactionPage />} />{" "}
             <Route path=":walletId/members" element={<AssignMemberPage />} />{" "}
-            <Route path=":walletId/history" element={<HistoryTransactionPage />} />
+            <Route
+              path=":walletId/history"
+              element={<HistoryTransactionPage />}
+            />
             <Route path=":walletId/transfer" element={<TransferPage />} />
           </Route>
-
-
           <Route path="allhistory" element={<AllHistoryPage />} />
-
-
           {/* ----------- Transfer Flow ----------- */}
           <Route
             path="transfer/*"
@@ -296,6 +295,6 @@ export default function AppRoutes() {
 
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter >
+    </BrowserRouter>
   );
 }
